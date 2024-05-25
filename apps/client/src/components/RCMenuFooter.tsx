f25c29e24e7ba22e0f9cfd45d18a3cf623764848
Serhii <gadzhilov.sergey@gmail.com>
2024-05-25 16:07:07 +0300

    Implement footer

--- apps/client/src/components/RCMenuFooter.tsx
+++ apps/client/src/components/RCMenuFooter.tsx
@@ -1,21 +1,10 @@
+import { Link } from "react-router-dom";
+
 const RCMenuFooter = () => {
     return (
         <footer>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="footer__content">
-                            <div className="footer__copyrigt_menu">
-                                <p>Copyright © {new Date().getFullYear()}</p>
-                                <ul>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                    <li><a href="#">Support</a></li>
-                                 </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Link className="terms_of_service" to="/">Terms of Service</Link>
+            <div>Copyright © {new Date().getFullYear()} All rights reserved</div>
         </footer>
     );
 };
