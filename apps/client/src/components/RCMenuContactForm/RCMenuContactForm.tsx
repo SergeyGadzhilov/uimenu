bd92e66b7d9fd22f78bc8bb9f233ca6a12a9edb9
Serhii <gadzhilov.sergey@gmail.com>
2024-05-26 14:03:49 +0300

    Add spinner into the form

--- apps/client/src/components/RCMenuContactForm/RCMenuContactForm.tsx
+++ apps/client/src/components/RCMenuContactForm/RCMenuContactForm.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import styles from './RCMenuContactFrom.module.css'
 
 const RCMenuContactFrom = () => {
+    const [isLoading, setLoading] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setLoading(true);
+    };
+
     return (
-        <form className={styles.form}>
-            <input className={styles.input} type="text" placeholder="Name"></input>
-            <input className={styles.input} type="text" placeholder="Email"></input>
-            <textarea className={styles.textarea} placeholder="Message"/>
-            <input className={styles.submit} type="submit" value="Send" />
+        <form className={styles.form} onSubmit={handleSubmit}>
+        {
+            isLoading ? <img className={styles.spinner} src="/assets/images/spinner.svg"></img> :
+            <>
+                <input className={styles.input} type="text" placeholder="Name"></input>
+                <input className={styles.input} type="text" placeholder="Email"></input>
+                <textarea className={styles.textarea} placeholder="Message"/>
+                <input className={styles.submit} type="submit" value="Send" />
+            </>
+        }
         </form>
     );
 };
