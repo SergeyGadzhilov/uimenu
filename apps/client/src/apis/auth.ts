3ad6577bde5c93a4950cf534e28091fa58a2bb31
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-05-28 13:27:01 +0300

    Fixed #32 Registration form (#45)

--- apps/client/src/apis/auth.ts
+++ apps/client/src/apis/auth.ts
@@ -1,7 +1,12 @@
 import { Login } from "../types";
 import { PostRequest } from "./common";
+import { CreateUserResponse, UserDTO } from "./dto/CreateUserDTO";
 import { LoginResponse, TokenDTO } from "./dto/TokenDTO";
 
 export async function LogIn(login: Login) : Promise<LoginResponse> {
     return await PostRequest<Login, TokenDTO>("/auth/login/", login);
+}
+
+export async function ApiRegister(login: Login) : Promise<CreateUserResponse> {
+    return await PostRequest<Login, UserDTO>("/users/", login);
 }
