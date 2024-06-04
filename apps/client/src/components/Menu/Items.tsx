6041b2f28ec2b28bd8381088de0abbe1f0b969e7
Serhii <gadzhilov.sergey@gmail.com>
2024-06-04 13:24:29 +0300

    Fixed #19 New design for the menu

--- apps/client/src/components/Menu/Items.tsx
+++ apps/client/src/components/Menu/Items.tsx
@@ -1,14 +1,38 @@
+import { createContext, useContext } from "react";
+import AccentButton from "../Buttons/buttons";
 import styles from "./items.module.css";
 
+const ItemsContext = createContext(null);
+
+function CategoryItem({item}) {
+    const onOrder = useContext(ItemsContext);
+
+    return (
+        <li className={styles.item}>
+            {item.image && <div className={styles.image} style={{backgroundImage: `url(${item.image})`}}></div>}
+            <div className={styles.item_data}>
+                <h3 className={styles.item_title}>{item.name}</h3>
+                <p className={styles.item_description}>{item.description}</p>
+                <p className={styles.item_price}>${item.price}</p>
+                <AccentButton onPress={() => onOrder(item)}>Add</AccentButton>
+            </div>
+        </li>
+    );
+}
+
 function CategoryItems({items = null}) {
-    return (<></>);
+    return (
+        <ul>
+            {items?.map((item) => <CategoryItem key={item.id} item={item}/>)}
+        </ul>
+    );
 }
 
 function Category({category}) {
     return (
         <>
             { category?.items?.length > 0 && (
-                <div>
+                <div id={category.id}>
                     <h3 className={styles.category_title}>{category.name}</h3>
                     <CategoryItems items={category?.items} />
                 </div>
@@ -17,10 +41,10 @@ function Category({category}) {
     );
 }
 
-export function Items({categories}) {
+export function Items({categories, onOrder = null}) {
     return (
-        <>
+        <ItemsContext.Provider value={onOrder}>
             {categories?.map((category) => <Category key={category.id} category={category}/>)}
-        </>
+        </ItemsContext.Provider>
     );
 }
--- apps/client/src/components/Menu/MenuList.tsx
+++ apps/client/src/components/Menu/MenuList.tsx
@@ -1,24 +1,11 @@
-import styled from 'styled-components';
-import MenuItem from './MenuItem';
 import { PlaceType } from '../../types';
 import { Categories } from './Categories';
 import { useState } from 'react';
 import { Place } from './Place';
 import { Items } from './Items';
 
-interface FontInterface{
-    font:string
-}
-
-const Container = styled.div<FontInterface>`
-  b, p {
-    ${({ font }) => font && `font-family: ${font};` }
-  }
-`;
-
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const MenuList = ({ place, shoppingCart = {}, onOrder, font,color }:{place:PlaceType,shoppingCart?:any,onOrder:(item:any)=>void,font?:string,color?:string}) => {
+const MenuList = ({ place, onOrder } : {place:PlaceType, onOrder:(item:any)=>void}) => {
   const [activeCategory, setActiveCategory] = useState(place?.categories[0]?.id);
   if(place?.categories?.length == 0){
     return <h4 className='text-center text-gray'>Please Add Menu Categories <br/> and menu items</h4>
@@ -30,37 +17,11 @@ const MenuList = ({ place, shoppingCart = {}, onOrder, font,color }:{place:Place
   }
 
   return (
-    <Container font={font!}>
+    <>
       <Place place={place}/>
       <Categories active={activeCategory} onChange={ChangeCategory} categories={place?.categories}/>
-      <Items categories={place?.categories} />
-
-      {place?.categories
-        ?.filter(
-          (category) => category?.items?.length
-        )
-        .map((category) => (
-          <div id={category?.id} key={category?.id} className="mt-5">
-            <h4 className="mb-4" style={{color}}>
-              <b>{category?.name}</b>
-            </h4>
-            {category?.items
-              .map((item) => (
-                <MenuItem
-                  key={item.id}
-                  item={{
-                    ...item,
-                    quantity: shoppingCart[item.id!]?.quantity,
-                  }} 
-                  onOrder={onOrder}
-                  color={color}
-                />
-              ))
-            }
-          </div>
-        ))
-      }
-    </Container>
+      <Items categories={place?.categories} onOrder={onOrder}/>
+    </>
   )
 };
 
