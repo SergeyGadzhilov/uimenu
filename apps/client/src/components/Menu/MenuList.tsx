52194f416b29e12db58dc217d0db18a4650a5293
Serhii <gadzhilov.sergey@gmail.com>
2024-06-04 12:04:12 +0300

    Show a place image only if the place has an image

--- apps/client/src/components/Menu/MenuList.tsx
+++ apps/client/src/components/Menu/MenuList.tsx
@@ -22,6 +22,14 @@ const Container = styled.div<FontInterface>`
   }
 `;
 
+function PlaceImage({place = null}) {
+  return (
+    <>
+      {place?.image && <img src={place?.image} width={100} height={100} alt={place?.name}/>}
+    </>
+  );
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const MenuList = ({ place, shoppingCart = {}, onOrder, font,color }:{place:PlaceType,shoppingCart?:any,onOrder:(item:any)=>void,font?:string,color?:string}) => {
   const [activeCategory, setActiveCategory] = useState(place?.categories[0]?.id);
@@ -37,8 +45,8 @@ const MenuList = ({ place, shoppingCart = {}, onOrder, font,color }:{place:Place
   return (
     <Container font={font!}>
       <Place>
-        <img src={place?.image} width={100} height={100} alt={place?.name}/>
-        <h3 style={{color:color}}><b>{place?.name}</b> </h3>
+        <PlaceImage place={place}/>
+        <h3 style={{color:color}}><b>{place?.name}</b></h3>
       </Place>
       <Categories active={activeCategory} onChange={ChangeCategory} place={place}/>
 
