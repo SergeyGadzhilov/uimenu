ef02b3a9ff192300d39134193e3dbd9460d79481
Serhii <gadzhilov.sergey@gmail.com>
2024-06-08 21:17:48 +0300

    Add aws docker

--- apps/client/src/apis/categories.ts
+++ apps/client/src/apis/categories.ts
@@ -2,12 +2,12 @@ import { DeleteRequest, PatchRequest, PostRequest } from "./common";
 import { CategoryDto, CreateCategoryResponse, DeleteCategoryResponse, UpdateCategoryResponse } from "./dto/CategoryDTO";
 
 export async function CreateCategory(category: CategoryDto, token: string = "") : Promise<CreateCategoryResponse> {
-    return await PostRequest<CategoryDto, CategoryDto>(`/places/${category.placeId}/categories/`, category, token);
+    return await PostRequest<CategoryDto, CategoryDto>(`places/${category.placeId}/categories/`, category, token);
 }
 
 export async function DeleteCategory(category: CategoryDto, token: string = "") : Promise<DeleteCategoryResponse> {
     return await DeleteRequest<CategoryDto, CategoryDto>(
-        `/places/${category.placeId}/categories/${category.id}`,
+        `places/${category.placeId}/categories/${category.id}`,
         category,
         token
     )
@@ -15,7 +15,7 @@ export async function DeleteCategory(category: CategoryDto, token: string = "") 
 
 export async function UpdateCategory(category: CategoryDto, token: string = "") : Promise<UpdateCategoryResponse> {
     return await PatchRequest<CategoryDto, CategoryDto>(
-        `/places/${category.placeId}/categories/${category.id}`,
+        `places/${category.placeId}/categories/${category.id}`,
         category,
         token
     );
--- apps/client/src/apis/places.ts
+++ apps/client/src/apis/places.ts
@@ -2,13 +2,13 @@ import { GetRequest, PatchRequest, PostRequest } from "./common";
 import { CreatePlaceRequest, CreatePlaceResponse, GetPlacesResponse, PlaceDTO, UpdatePlaceResponse } from "./dto/PlacesDTO";
 
 export async function CreatePlace(request: CreatePlaceRequest, token: string = "") : Promise<CreatePlaceResponse> {
-    return await PostRequest<CreatePlaceRequest, PlaceDTO>("/places/", request, token);
+    return await PostRequest<CreatePlaceRequest, PlaceDTO>("places/", request, token);
 }
 
 export async function GetPlaces(token: string = "") : Promise<GetPlacesResponse> {
-    return await GetRequest<string, PlaceDTO[]>(`/places/`, null, token);
+    return await GetRequest<string, PlaceDTO[]>(`places/`, null, token);
 }
 
 export async function UpdatePlace(place: PlaceDTO = null, token:string = "") : Promise<UpdatePlaceResponse> {
-    return await PatchRequest<PlaceDTO, PlaceDTO>(`/places/${place.id}`, place, token);
+    return await PatchRequest<PlaceDTO, PlaceDTO>(`places/${place.id}`, place, token);
 }
