56c2456b51315c57336db9065a640c15a3ff7634
Serhii <gadzhilov.sergey@gmail.com>
2024-06-10 10:27:38 +0300

    Fixed #73 Remove subscribe button from the price section

--- apps/client/src/components/RCMenuPriceCard.tsx
+++ apps/client/src/components/RCMenuPriceCard.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 const RCMenuPriceCard = ({title, descirption, price}) => {
     return (
         <div className="price_card">
@@ -7,7 +5,6 @@ const RCMenuPriceCard = ({title, descirption, price}) => {
             <div className="price_card__description">{descirption}</div>
             <div className="price_card__price">{price}</div>
             <div className="price_card__period">per month</div>
-            <Link className="price_card__button" to="/">Subscribe</Link>
         </div>
     );
 };
