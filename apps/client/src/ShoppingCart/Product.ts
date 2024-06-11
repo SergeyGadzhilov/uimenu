0d5ce4a4fc22756919f61174888bdc6e7fc3f684
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-06-11 20:29:12 +0300

    Fixed #106 Fix shopping cart (#113)

---
+++ apps/client/src/ShoppingCart/Product.ts
@@ -0,0 +1,10 @@
+export  class Product {
+    id:          string = "";
+    name:        string = "";
+    image:       string = "";
+    price:       number = 0;
+    categoryId:  string = "";
+    description: string = "";
+    isAvailable: boolean = true;
+    quantity:    number = 0;
+}
