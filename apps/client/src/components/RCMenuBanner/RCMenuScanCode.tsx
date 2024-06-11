a61a9b9c037377cc1445eefa2b47fc36db6a06f5
Serhii <gadzhilov.sergey@gmail.com>
2024-06-11 12:38:09 +0300

    Fixed #76 Create demo menu

--- apps/client/src/components/RCMenuBanner/RCMenuScanCode.tsx
+++ apps/client/src/components/RCMenuBanner/RCMenuScanCode.tsx
@@ -7,7 +7,7 @@ export default function RCMenuScanCode() {
               <img src="/qr.png" alt="QR_Code" />
               <div className={styles.description}>Scan me <br/>to try demo</div>
             </div>
-            or <a className={styles.link} href="" target="_blank" rel="noopener">Click here</a>
+            or <a className={styles.link} href="/demo" target="_blank" rel="noopener">Click here</a>
         </div>
     );
 };
