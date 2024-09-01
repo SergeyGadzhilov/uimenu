f7780b7ff0235ba45e5745053fabec00cff87956
Serhii Hadzhilov <gadzhilov.sergey@gmail.com>
2024-09-01 17:48:24 +0300

    Fixed #121 Remove section prices

--- apps/client/src/pages/IndexPage.tsx
+++ apps/client/src/pages/IndexPage.tsx
@@ -2,7 +2,6 @@ import UIMenuHeader from "../components/UIMenuHeader/UIMenuHeader";
 import UIMenuBanner from "../components/UIMenuBanner/UIMenuBanner";
 import UIMenuEditorSection from "../components/UIMenuEditorSection";
 import UIMenuFooter from "../components/UIMenuFooter";
-import UIMenuPlansAndPrices from "../components/UIMenuPlansAndPrices";
 import UIMenuContactsSection from "../sections/contacts/UIMenuContactsSection";
 
 function IndexPage() {
@@ -11,7 +10,6 @@ function IndexPage() {
       <UIMenuHeader/>
       <UIMenuBanner/>
       <UIMenuEditorSection/>
-      <UIMenuPlansAndPrices/>
       <UIMenuContactsSection/>
       <UIMenuFooter/>
     </div>
