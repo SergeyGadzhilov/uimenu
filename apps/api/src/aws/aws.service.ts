faa959fb765555949d4b868bf9eca4d712a04921
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-10-01 14:48:27 +0300

    Fixed #128 remove passwords from the repo (#135)
    * Remove aws docker compose
    
    * Remove passwords from the repo
    
    * Fixed #128 Remove credentials

--- apps/api/src/aws/aws.service.ts
+++ apps/api/src/aws/aws.service.ts
@@ -1,24 +1,27 @@
-import { Injectable } from "@nestjs/common";
-import { GetUploadUrlRequest } from "./aws.dto";
-import { PutObjectCommand, S3Client, S3ClientConfig} from "@aws-sdk/client-s3"
-import {getSignedUrl} from "@aws-sdk/s3-request-presigner"
+import { Injectable } from '@nestjs/common';
+import { GetUploadUrlRequest } from './aws.dto';
+import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 @Injectable()
 export class AwsService {
-    s3Configuration: S3ClientConfig = {
-        region: "us-west-1",
-        credentials: {
-            accessKeyId: "AKIAZI2LI6P3HJ5IOCPH",
-            secretAccessKey: "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"
-        }
-    }
-    _aws: S3Client = new S3Client(this.s3Configuration);
+  s3Configuration: S3ClientConfig = {
+    region: '',
+    credentials: {
+      accessKeyId: '',
+      secretAccessKey: '',
+    },
+  };
+  _aws: S3Client = new S3Client(this.s3Configuration);
 
-    async GetSecureUrl(userid: string, request: GetUploadUrlRequest) : Promise<string> {
-        const command = new PutObjectCommand({
-            Bucket: "rc-menu-storage",
-            Key: `${userid}/${request.menu ?? "places"}/${request.image.path}-${Date.now()}`
-        });
-        return await getSignedUrl(this._aws, command, {expiresIn: 3600});
-    }
+  async GetSecureUrl(
+    userid: string,
+    request: GetUploadUrlRequest,
+  ): Promise<string> {
+    const command = new PutObjectCommand({
+      Bucket: 'rc-menu-storage',
+      Key: `${userid}/${request.menu ?? 'places'}/${request.image.path}-${Date.now()}`,
+    });
+    return await getSignedUrl(this._aws, command, { expiresIn: 3600 });
+  }
 }
