14df5fb22e2c7e205bc141fa6d14a26d0a825d41
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-11-30 13:01:28 +0200

    Fixed #139 Update the menu editor section with the new image (#164)
    * Add the new image and refactoring
    
    * Fixed #139 Update the menu editor image

--- apps/client/src/pages/IndexPage.tsx
+++ apps/client/src/pages/IndexPage.tsx
@@ -1,6 +1,6 @@
 import UIMenuHeader from "../components/UIMenuHeader/UIMenuHeader";
 import UIMenuBanner from "../components/UIMenuBanner/UIMenuBanner";
-import UIMenuEditorSection from "../components/UIMenuEditorSection";
+import UIMenuEditorSection from "../components/UIMenuEditorSection/UIMenuEditorSection";
 import UIMenuFooter from "../components/UIMenuFooter";
 import UIMenuContactsSection from "../sections/contacts/UIMenuContactsSection";
 
