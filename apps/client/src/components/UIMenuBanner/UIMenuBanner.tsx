dde2a00110010ad0570d5e03f2e341e4aca6192d
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-12-01 11:24:15 +0200

    Fixed #141 Update the main page banner (#165)

--- apps/client/src/components/UIMenuBanner/UIMenuBanner.tsx
+++ apps/client/src/components/UIMenuBanner/UIMenuBanner.tsx
@@ -7,11 +7,11 @@ const UIMenuBanner = () => {
     return (
         <section className={styles.banner}>
           <div className={styles.left} >
-            <h2 className={styles.title}>Contactless digital <br/>QR Code Menu</h2>
+            <h2 className={styles.title}>Free digital menu for hotels,<br/>cafes and restaurants</h2>
             <div className={styles.description}>
               <div>
-                Revolutionize your customers' ordering process with our contactless QR Code menu solution.
-                <span className={styles.description2}> Create digital restaurant menu and share it via a unique QR code with just a few clicks.</span>
+                The simplest way to create digital menu for your business.
+                <span className={styles.description2}> Create digital menu and share it via a unique QR code with just a few clicks.</span>
               </div>
             </div>
             <div className={styles.button}>
