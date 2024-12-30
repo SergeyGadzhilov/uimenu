8601e68086c7d983caf71ab20c4d0df477884c73
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-12-30 09:57:22 +0200

    Fixed #170 Add support link to the main page (#171)

--- apps/client/src/sections/contacts/UIMenuContactsSection.tsx
+++ apps/client/src/sections/contacts/UIMenuContactsSection.tsx
@@ -6,7 +6,7 @@ const UIMenuContactsSection = () => {
         <section className={styles.contacts}>
             <div className={styles.title}>Contact Us</div>
             <div className={styles.description}>
-                If you have any questions, simply send us a message, and we will get back to you as soon as possible
+                If you have any questions, simply send us a message to <a href="mailto:support@uimenu.com">support@uimenu.com</a> or using the form bellow, and we will get back to you as soon as possible
             </div>
             <div className={styles.form}>
                 <UIMenuContactFrom/>
