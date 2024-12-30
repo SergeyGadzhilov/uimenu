a318c05abbcc1e18033ddc111cacad5f7f3c031a
SergeyGadzhilov <gadzhilov.sergey@gmail.com>
2024-12-30 09:39:15 +0200

    Fixed #168 Contact form on the the main page should send email to support (#169)
    * Fixed #168 Send email from the contact form
    
    * Fix template

--- apps/api/src/support/support.service.ts
+++ apps/api/src/support/support.service.ts
@@ -1,13 +1,26 @@
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateTicketDTO } from "./dto/create-ticket.dto";
 import { Injectable } from "@nestjs/common";
+import { SendEmail } from "src/email/mailer";
 
 @Injectable()
 export class  SupportService {
     constructor(private prisma: PrismaService) {
     }
 
-    create(ticket: CreateTicketDTO) {
+    async create(ticket: CreateTicketDTO) {
+        await SendEmail({
+            to: "support@uimenu.com",
+            from: "UIMenu <no-reply@uimenu.com>",
+            subject: `Email from user ${ticket.name}`,
+            template: "user_email",
+            data: {
+                user_email: ticket.email,
+                user_name: ticket.name,
+                user_message: ticket.message
+            },
+        });
+
         return this.prisma.supportTicket.create({
             data: {...ticket}
         });
